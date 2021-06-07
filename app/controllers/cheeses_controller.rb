0ba04c71d43af1d832809f9d8f51b5bf2a18a74f
Charles Grant Nichols <grant@inkdots.com>
2021-06-07 16:30:15 -0500

    commit tree 024f71a2397f56b723c01ff3505fb1f12abdf615

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,12 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    #find a cheese using the id from URL
+   cheese = Cheese.find_by(id:params[:id])
+    # want a json response using the cheese object
+    render json: cheese
+  end
+
+
 end
